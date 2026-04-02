92e045742dfed065def3a58c8901c50ff045d7d4
krasnal0 <d.owczarek2007@gmail.com>
2026-04-03 00:50:29 +0200

    Update CertificatesSection.tsx

--- src/components/CertificatesSection.tsx
+++ src/components/CertificatesSection.tsx
@@ -6,13 +6,15 @@ import cert2 from '@/assets/certyfikat-2.png';
 import cert3 from '@/assets/certyfikat-3.png';
 import cert4 from '@/assets/certyfikat-4.png';
 import cert5 from '@/assets/certyfikat-5.png';
+import cert5 from '@/assets/certyfikat-5.jpg';
 
 const certificates = [
   { image: cert1, title: 'Certyfikat – Knowledge & Perception of Personal Training (REPs Polska)' },
   { image: cert2, title: 'Certyfikat – Anatomy & Physiology (REPs Polska)' },
   { image: cert3, title: 'Certyfikat – Układanie planu treningowego cz. 1 (PLTP)' },
   { image: cert4, title: 'Certyfikat – Układanie planu treningowego cz. 2 (PLTP)' },
-   { image: cert5, title: 'Certyfikat – Układanie planu treningowego cz. 2 (PLTP)' },
+  { image: cert5, title: 'Certyfikat – Dojebane Plecy  (PLTP)' },
+  { image: cert5, title: 'Certyfikat – Trener Perosnalny (PLTP)' },
 ];
 
 const CertificatesSection = () => {
